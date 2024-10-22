860f1c7ccb92de25b5776f286d88e17d0413a06d
tiero <3596602+tiero@users.noreply.github.com>
2024-10-23 00:13:32 +0200

    Add Capacitor and ios target

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'to.arklabs.arkade',
+  appName: 'arkade',
+  webDir: 'build'
+};
+
+export default config;
