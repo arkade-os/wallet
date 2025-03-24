169220d10f048209563c546c1007cede0024e2a7
João Bordalo <bordalix@users.noreply.github.com>
2025-03-24 15:39:10 +0000

    don't use ionic icons

--- src/screens/Wallet/Onboard.tsx
+++ src/screens/Wallet/Onboard.tsx
@@ -10,76 +10,12 @@ import Padded from '../../components/Padded'
 import Button from '../../components/Button'
 import ButtonsOnBottom from '../../components/ButtonsOnBottom'
 import { IonIcon } from '@ionic/react'
-import { shareOutline, add } from 'ionicons/icons'
 import Bullet from '../../components/Bullet'
 import FlexCol from '../../components/FlexCol'
 import FlexRow from '../../components/FlexRow'
 import Shadow from '../../components/Shadow'
-
-const OnboardInfo = ({ step }: { step: number }): ReactNode => {
-  if (step === 1) {
-    return (
-      <FlexCol gap='0.5rem'>
-        <Title text='Welcome to Arkade' />
-        <Text wrap>
-          Begin using Bitcoin in a faster, more efficient way. Arkade creates a private space where your transactions
-          happen instantly without waiting for network confirmation.
-        </Text>
-      </FlexCol>
-    )
-  }
-  if (step === 2) {
-    return (
-      <FlexCol gap='0.5rem'>
-        <Title text='Stay in control' />
-        <Text wrap>
-          Your Bitcoin remains yours at all times. Choose when to move transactions to the main Bitcoin network to save
-          on fees, with the freedom to withdraw whenever you want.
-        </Text>
-      </FlexCol>
-    )
-  }
-  if (step === 3) {
-    return (
-      <FlexCol gap='0.5rem'>
-        <Title text='Connect seamlessly' />
-        <Text wrap>
-          Easily bundle your transactions for secure settlement on the Bitcoin network. Enjoy smart features and
-          functions while maintaining Bitcoin's core security benefits.
-        </Text>
-      </FlexCol>
-    )
-  }
-  if (step === 4) {
-    return (
-      <FlexCol gap='0.5rem'>
-        <Title text='Install Arkade on Home' />
-        <Text wrap>Adding Arkade to Home enable push notifications and better user experience.</Text>
-        <Shadow>
-          <FlexCol gap='1rem'>
-            <FlexRow>
-              <Bullet number={1} />
-              <Text>Tap</Text>
-              <Shadow flex inverted slim>
-                <IonIcon icon={shareOutline} style={{ color: 'var(--ion-background-color)' }} />
-              </Shadow>
-            </FlexRow>
-            <FlexRow>
-              <Bullet number={2} />
-              <Text>Then</Text>
-              <Shadow flex inverted slim>
-                <FlexRow>
-                  <Text color='ion-background-color'>Add to Home Screen</Text>
-                  <IonIcon icon={add} style={{ color: 'var(--ion-background-color)' }} />
-                </FlexRow>
-              </Shadow>
-            </FlexRow>
-          </FlexCol>
-        </Shadow>
-      </FlexCol>
-    )
-  }
-}
+import AddIcon from '../../icons/Add'
+import ShareIcon from '../../icons/Share'
 
 export default function Onboard() {
   const { navigate } = useContext(NavigationContext)
@@ -91,20 +27,13 @@ export default function Onboard() {
 
   const handleSkip = () => navigate(Pages.Init)
 
-  const Image = () => {
-    switch (step) {
-      case 1:
-        return <OnboardImage1 />
-      case 2:
-        return <OnboardImage2 />
-      case 3:
-        return <OnboardImage3 />
-      default:
-        return <OnboardImage4 />
-    }
-  }
-
   const ImageContainer = () => {
+    const Image = () => {
+      if (step === 1) return <OnboardImage1 />
+      if (step === 2) return <OnboardImage2 />
+      if (step === 3) return <OnboardImage3 />
+      return <OnboardImage4 />
+    }
     const style: any = {
       alignItems: 'center',
       display: 'flex',
@@ -119,6 +48,64 @@ export default function Onboard() {
     )
   }
 
+  const InfoContainer = (): ReactNode => {
+    const info = ({ title, text }: { title: string; text: string }): ReactNode => {
+      return (
+        <FlexCol gap='0.5rem'>
+          <Title text={title} />
+          <Text wrap>{text}</Text>
+        </FlexCol>
+      )
+    }
+    if (step === 1) {
+      return info({
+        title: 'Welcome to Arkade',
+        text: 'Begin using Bitcoin in a faster, more efficient way. Arkade creates a private space where your transactions happen instantly without waiting for network confirmation.',
+      })
+    }
+    if (step === 2) {
+      return info({
+        title: 'Stay in control',
+        text: 'Your Bitcoin remains yours at all times. Choose when to move transactions to the main Bitcoin network to save on fees, with the freedom to withdraw whenever you want.',
+      })
+    }
+    if (step === 3) {
+      return info({
+        title: 'Connect seamlessly',
+        text: "Easily bundle your transactions for secure settlement on the Bitcoin network. Enjoy smart features and functions while maintaining Bitcoin's core security benefits.",
+      })
+    }
+    if (step === 4) {
+      return (
+        <FlexCol gap='0.5rem'>
+          <Title text='Install Arkade on Home' />
+          <Text wrap>Adding Arkade to Home enable push notifications and better user experience.</Text>
+          <Shadow>
+            <FlexCol gap='1rem'>
+              <FlexRow>
+                <Bullet number={1} />
+                <Text>Tap</Text>
+                <Shadow flex inverted slim>
+                  <ShareIcon reversed />
+                </Shadow>
+              </FlexRow>
+              <FlexRow>
+                <Bullet number={2} />
+                <Text>Then</Text>
+                <Shadow flex inverted slim>
+                  <FlexRow>
+                    <Text color='ion-background-color'>Add to Home Screen</Text>
+                    <AddIcon reversed />
+                  </FlexRow>
+                </Shadow>
+              </FlexRow>
+            </FlexCol>
+          </Shadow>
+        </FlexCol>
+      )
+    }
+  }
+
   return (
     <>
       <Content>
@@ -126,7 +113,7 @@ export default function Onboard() {
           <FlexCol between>
             <StepBars active={step} length={steps} />
             <ImageContainer />
-            <OnboardInfo step={step} />
+            <InfoContainer />
           </FlexCol>
         </Padded>
       </Content>
