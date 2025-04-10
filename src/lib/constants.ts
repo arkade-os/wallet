f960b4579b7478bc24ab136a0395e12c0f4537ba
tiero <3596602+tiero@users.noreply.github.com>
2025-04-11 01:15:22 +0200

    arkd instance
    Signed-off-by: tiero <3596602+tiero@users.noreply.github.com>

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const arknoteHRP = 'arknote'
 export const defaultFee = 0
 export const testDomain = 'dev.arkade.money'
 export const testServer = 'https://master.mutinynet.arklabs.to'
-export const mainServer = 'https://mutinynet.arkade.sh'
+export const mainServer = 'https://bitcoin.arkade.sh'
 
 export const defaultArkServer = () => {
   return window.location.hostname === testDomain ? testServer : mainServer
