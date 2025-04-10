0c9699032c138ae64872070a21d8c2c091bf2d48
João Bordalo <bordalix@users.noreply.github.com>
2025-04-10 23:20:19 +0100

    move submarineSwap() to lib/boltz

--- src/screens/Wallet/SendLightning/Form.tsx
+++ src/screens/Wallet/SendLightning/Form.tsx
@@ -9,16 +9,18 @@ import Scanner from '../../../components/Scanner'
 import Error from '../../../components/Error'
 import ButtonsOnBottom from '../../../components/ButtonsOnBottom'
 import Button from '../../../components/Button'
-import { sendOffChain } from '../../../lib/asp'
-
-const boltzApi = 'http://localhost:9006'
+import { WalletContext } from '../../../providers/wallet'
+import { getPubKey, submarineSwap } from '../../../lib/boltz'
+import { FlowContext } from '../../../providers/flow'
 
 export default function SendLightningForm() {
+  const { sendInfo, setSendInfo } = useContext(FlowContext)
   const { navigate } = useContext(NavigationContext)
+  const { wallet } = useContext(WalletContext)
 
+  const [error, setError] = useState('')
   const [recipient, setRecipient] = useState('')
   const [scan, setScan] = useState(false)
-  const [error, setError] = useState('')
 
   const handleContinue = async () => {
     if (!recipient) {
@@ -27,36 +29,11 @@ export default function SendLightningForm() {
     }
 
     try {
-      const response = await fetch(`${boltzApi}/v2/swap/submarine`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          from: 'ARK',
-          to: 'BTC',
-          invoice: recipient,
-          // TODO: get this out of "window.receive"?
-          refundPublicKey: '03c27f5aa8943bad4e18285689904e9b30e58bc51923e920f394b5e55d7585a5e9',
-        }),
-      })
-
-      if (!response.ok) {
-        const errorData = await response.json()
-        setError(errorData.error || 'Failed to process Lightning payment')
-        return
-      }
-
-      const res = (await response.json()) as {
-        address: string
-        expectedAmount: number
-      }
-      console.log('Swap created:', res)
-
-      const sendRes = await sendOffChain(res.expectedAmount, res.address)
-      console.log('Send response:', sendRes)
-
+      const pubkey = await getPubKey()
+      const { txid, amount } = await submarineSwap(recipient, pubkey, wallet)
       // TODO: use websocket or check /swap/<id> to show pending/success states
+      setSendInfo({ ...sendInfo, total: amount, txid })
+      navigate(Pages.SendSuccess)
     } catch (err) {
       console.error('Error creating Lightning payment:', err)
       setError('Failed to connect to payment server')
@@ -77,7 +54,9 @@ export default function SendLightningForm() {
               label='Invoice'
               onChange={setRecipient}
               onEnter={() => {}}
-              openScan={() => {}}
+              openScan={() => {
+                setScan(true)
+              }}
               value={recipient}
             />
           </FlexCol>
