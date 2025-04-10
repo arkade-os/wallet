a10ba44a01194865cdf4077ab1e71202a57e2d20
João Bordalo <bordalix@users.noreply.github.com>
2025-04-10 16:24:08 +0100

    add fuji money

--- src/screens/Apps.tsx
+++ src/screens/Apps.tsx
@@ -4,7 +4,7 @@ import FlexRow from '../components/FlexRow'
 import Padded from '../components/Padded'
 import Header from '../components/Header'
 import FlexCol from '../components/FlexCol'
-import Text from '../components/Text'
+import Text, { TextSecondary } from '../components/Text'
 import Shadow from '../components/Shadow'
 import InvadersIcon from '../icons/Invaders'
 import CoinflipIcon from '../icons/Coinflip'
@@ -17,23 +17,16 @@ interface AppProps {
   name: string
 }
 
-function App({ desc, icon, link, name }: AppProps) {
+function App({ icon, link, name }: AppProps) {
   return (
     <Shadow onClick={() => link && window.open(link, '_blank')}>
-      <FlexCol gap='.5rem'>
-        <FlexRow>
-          {icon}
-          <FlexCol gap='0'>
-            <Text>{name}</Text>
-            <Text color='dark80' small>
-              {link ?? 'COMING SOON'}
-            </Text>
-          </FlexCol>
-        </FlexRow>
-        <Text color='dark80' small>
-          {desc}
-        </Text>
-      </FlexCol>
+      <FlexRow>
+        {icon}
+        <FlexCol gap='0'>
+          <Text>{name}</Text>
+          <TextSecondary>COMING SOON</TextSecondary>
+        </FlexCol>
+      </FlexRow>
     </Shadow>
   )
 }
