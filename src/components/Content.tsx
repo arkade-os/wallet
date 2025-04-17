4a71f8cb96b4f2ac2212ff4f60aa3e34c6d81581
louisinger <louis@vulpem.com>
2025-04-17 14:41:36 +0200

    remove IonRefresher

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,17 +1,13 @@
-import { IonContent, IonRefresher, IonRefresherContent } from '@ionic/react'
-import { ReactNode, useRef } from 'react'
+import { IonContent } from '@ionic/react'
+import { ReactNode } from 'react'
+
 interface ContentProps {
   children: ReactNode
 }
 
 export default function Content({ children }: ContentProps) {
-  const refresher = useRef<HTMLIonRefresherElement>(null)
-
   return (
     <IonContent>
-      <IonRefresher ref={refresher} slot='fixed'>
-        <IonRefresherContent />
-      </IonRefresher>
       <div style={{ height: '100%', paddingTop: '2rem' }}>{children}</div>
     </IonContent>
   )
