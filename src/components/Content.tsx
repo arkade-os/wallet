76de21a00d2052a46681a96adacb02b95ab7ba0d
João Bordalo <bordalix@users.noreply.github.com>
2025-06-23 11:13:23 +0100

    adds pull-to-refresh

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,6 @@
-import { IonContent } from '@ionic/react'
 import { ReactNode } from 'react'
+import Refresher from './Refresher'
+import { IonContent } from '@ionic/react'
 
 interface ContentProps {
   children: ReactNode
@@ -8,6 +9,7 @@ interface ContentProps {
 export default function Content({ children }: ContentProps) {
   return (
     <IonContent>
+      <Refresher />
       <div style={{ height: '100%', paddingTop: '2rem' }}>{children}</div>
     </IonContent>
   )
