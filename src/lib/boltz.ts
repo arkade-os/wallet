3a600f8be36955bcb0547424e3ed0d3e6e7cca32
João Bordalo <bordalix@users.noreply.github.com>
2025-07-10 11:44:48 +0100

    update ripemd160 import

--- src/lib/boltz.ts
+++ src/lib/boltz.ts
@@ -4,7 +4,7 @@ import { NetworkName } from '@arkade-os/sdk/dist/types/networks'
 import { Wallet } from './types'
 import { base64, hex } from '@scure/base'
 import { sha256 } from '@noble/hashes/sha2'
-import { ripemd160 } from '@noble/hashes/ripemd160'
+import { ripemd160 } from '@noble/hashes/legacy'
 import { randomBytes } from '@noble/hashes/utils'
 import {
   addConditionWitness,
