6d77adf78f394b6e966e6c5d34981302eb6648d0
louisinger <louis@vulpem.com>
2025-07-18 16:13:33 +0200

    add pages.dev to test server domains

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const arknoteHRP = 'arknote'
 export const defaultFee = 0
-export const testDomains = ['dev.arkade.money', 'next.arkade.money', 'localhost']
+export const testDomains = ['dev.arkade.money', 'next.arkade.money', 'pages.dev', 'localhost']
 export const devServer = 'http://localhost:7070'
 export const testServer = 'https://signet.arkade.sh'
 export const mainServer = 'https://mutinynet.arkade.sh'
