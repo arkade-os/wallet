0f6cd240e6ff5cb0e95dac6f56b7522e4713fd3d
louisinger <louis@vulpem.com>
2025-07-18 15:46:07 +0200

    update test server to signet

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const arknoteHRP = 'arknote'
 export const defaultFee = 0
 export const testDomains = ['dev.arkade.money', 'next.arkade.money', 'localhost']
 export const devServer = 'http://localhost:7070'
-export const testServer = 'https://staging.mutinynet.arkade.sh'
+export const testServer = 'https://signet.arkade.sh'
 export const mainServer = 'https://mutinynet.arkade.sh'
 
 export const defaultArkServer = () => {
