7b4f4a04bb1455dd196071f54c54d6f8ec5bb6f4
João Bordalo <bordalix@users.noreply.github.com>
2025-08-07 19:05:18 +0100

    change default test server

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const arknoteHRP = 'arknote'
 export const defaultFee = 0
 export const testDomains = ['dev.arkade.money', 'next.arkade.money', 'pages.dev', 'localhost']
 export const devServer = 'http://localhost:7070'
-export const testServer = 'https://mutinynet.arkade.sh'
+export const testServer = 'https://bitcoin-beta.arkade.sh'
 export const mainServer = 'https://bitcoin-beta.arkade.sh'
 export const isRiga = (import.meta.env.VITE_IS_RIGA as boolean) ?? false
 
