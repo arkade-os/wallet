1f3eedbda68868b5df28e277579f8fd62d0c435b
Andrew Camilleri (Kukks) <evilkukka@gmail.com>
2025-09-02 09:30:22 +0200

    format

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -19,5 +19,5 @@ export const defaultArkServer = () => {
 }
 
 export const BoltzUrl = (): string | undefined => {
-  return import.meta.env.VITE_BOLTZ_URL;
+  return import.meta.env.VITE_BOLTZ_URL
 }
