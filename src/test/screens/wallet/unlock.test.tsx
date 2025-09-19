5449d70dc4c2d074754682c2ec9feceb002cb4f9
João Bordalo <bordalix@users.noreply.github.com>
2025-09-19 10:25:20 +0100

    fix merge conflicts

--- src/test/screens/wallet/unlock.test.tsx
+++ src/test/screens/wallet/unlock.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import { describe, expect, it, vi } from 'vitest'
 import Unlock from '../../../screens/Wallet/Unlock'
-import userEvent from '@testing-library/user-event'
 import { IonApp } from '@ionic/react'
 
 let pwaCanInstall = false
