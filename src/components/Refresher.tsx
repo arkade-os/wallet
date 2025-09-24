374e3fc8cfeda847128ea259196be6754350726e
João Bordalo <bordalix@users.noreply.github.com>
2025-09-24 15:16:10 +0100

    use new svcWallet.reload() to reload

--- src/components/Refresher.tsx
+++ src/components/Refresher.tsx
@@ -3,16 +3,10 @@ import { WalletContext } from '../providers/wallet'
 import { useContext } from 'react'
 
 export default function Refresher() {
-  const { reloadWallet } = useContext(WalletContext)
+  const { reloadWallet, svcWallet } = useContext(WalletContext)
 
   const handleRefresh = async (event: { detail: { complete(): void } }) => {
-    if (navigator.serviceWorker.controller) {
-      navigator.serviceWorker.controller.postMessage({
-        type: 'RELOAD_WALLET',
-      })
-    } else {
-      console.log('No active service worker found.')
-    }
+    await svcWallet?.reload()
     await reloadWallet()
     event.detail.complete()
   }
