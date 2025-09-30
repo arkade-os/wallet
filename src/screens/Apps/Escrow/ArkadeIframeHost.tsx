c8d5f16c689c71a5b5ee5846d0eaf3ea25a9e112
Pietro Grandi <pietro@bluelabs.eu>
2025-10-01 00:42:39 +0200

    signin, orderbook

---
+++ src/screens/Apps/Escrow/ArkadeIframeHost.tsx
@@ -0,0 +1,95 @@
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { base64 } from '@scure/base'
+import makeMessageHandler from './RpcHandler'
+import { bytesToHex } from '@noble/hashes/utils'
+
+export type ArkadeIdentityHandlers = {
+  xOnlyPublicKey: () => Promise<Uint8Array | null>
+  sign: (tx: unknown, inputIndexes?: number[]) => Promise<unknown>
+  signerSession: () => Promise<unknown>
+  signin: (challenge: string) => Promise<string>
+  signout: () => Promise<unknown>
+}
+
+type Props = {
+  src: string
+  allowedChildOrigins: string[]
+  handlers: ArkadeIdentityHandlers
+}
+
+export const ArkadeIframeHost: React.FC<Props> = ({ src, allowedChildOrigins, handlers }) => {
+  const iframeRef = useRef<HTMLIFrameElement | null>(null)
+  const allowed = useMemo(() => new Set(allowedChildOrigins), [allowedChildOrigins])
+  const [isAlive, setIsAlive] = useState(false)
+  const handleMessage = useMemo(
+    () =>
+      makeMessageHandler({
+        getXPublicKey: () => handlers.xOnlyPublicKey().then((_) => (_ ? bytesToHex(_) : null)),
+        signLoginChallenge: (challenge: string) => handlers.signin(challenge),
+      }),
+    [handlers],
+  )
+
+  const poll = useCallback(() => {
+    if (iframeRef.current?.contentWindow) {
+      console.log(`[wallet] sending keep alive to ${src}...`)
+      iframeRef.current.contentWindow.postMessage({ kind: 'ARKADE_KEEP_ALIVE', timestamp: Date.now() }, src)
+      setTimeout(() => poll(), 5000)
+    }
+  }, [isAlive, iframeRef])
+
+  useEffect(() => {
+    const onMessage = async (event: MessageEvent) => {
+      if (!allowed.has(event.origin)) {
+        console.warn(`[wallet]: ignoring message from ${event.origin}`)
+        return
+      }
+      const msg = event.data
+      if (!msg || typeof msg !== 'object' || !('kind' in msg)) {
+        console.error('[wallet]: invalid message', msg)
+        return
+      }
+
+      try {
+        const result = await handleMessage(msg)
+        if (result.tag === 'failure') {
+          console.error(result.error)
+        } else {
+          if (!isAlive) {
+            console.log(`[wallet] setting isAlive to true`)
+            setIsAlive(true)
+          }
+          if (iframeRef.current?.contentWindow && result.result.kind !== 'ARKADE_KEEP_ALIVE') {
+            // we don't answer to keep alive messages because we send every 5s
+            console.log(`[wallet] sending result ${result.result.kind} to ${event.origin}`)
+            iframeRef.current.contentWindow.postMessage(result.result, event.origin)
+          }
+        }
+      } catch (err) {
+        console.error('[wallet]: error handling message', err)
+        return
+      }
+    }
+
+    window.addEventListener('message', onMessage)
+
+    return () => {
+      window.removeEventListener('message', onMessage)
+    }
+  }, [allowed, handlers, isAlive])
+
+  useEffect(() => {
+    poll()
+  }, [])
+
+  return (
+    <iframe
+      ref={iframeRef}
+      title='Ark Escrow'
+      src={src}
+      style={{ width: '100%', height: `100%`, border: 0, display: 'flex' }}
+      sandbox='allow-scripts allow-forms allow-same-origin'
+      allow='clipboard-read; clipboard-write'
+    />
+  )
+}
