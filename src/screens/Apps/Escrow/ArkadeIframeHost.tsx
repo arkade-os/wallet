71d87032a205ea486cfe300bbfbb74ce79f900b7
Pietro Grandi <pietro@bluelabs.eu>
2025-10-02 16:57:34 +0200

    things working

--- src/screens/Apps/Escrow/ArkadeIframeHost.tsx
+++ src/screens/Apps/Escrow/ArkadeIframeHost.tsx
@@ -1,13 +1,16 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import makeMessageHandler from './RpcHandler'
 import { bytesToHex } from '@noble/hashes/utils'
+import { Transaction } from '@scure/btc-signer'
+import { base64 } from '@scure/base'
 
 export type ArkadeIdentityHandlers = {
   xOnlyPublicKey: () => Promise<Uint8Array | null>
-  sign: (tx: unknown, inputIndexes?: number[]) => Promise<unknown>
+  sign(tx: Transaction, inputIndexes?: number[]): Promise<Transaction>
   signerSession: () => Promise<unknown>
   signin: (challenge: string) => Promise<string>
   signout: () => Promise<unknown>
+  getArkWalletAddress: () => Promise<string>
 }
 
 type Props = {
@@ -25,13 +28,29 @@ export const ArkadeIframeHost: React.FC<Props> = ({ src, allowedChildOrigins, ha
       makeMessageHandler({
         getXPublicKey: () => handlers.xOnlyPublicKey().then((_) => (_ ? bytesToHex(_) : null)),
         signLoginChallenge: (challenge: string) => handlers.signin(challenge),
+        getArkWalletAddress: () => handlers.getArkWalletAddress(),
+        signTransaction: async (base64Tx: string, base64Checkpoints: string[]) => {
+          const tx = Transaction.fromPSBT(base64.decode(base64Tx))
+          const checkpoints = base64Checkpoints.map((_) => base64.decode(_))
+          const signedTx = await handlers.sign(tx)
+          const signedCheckpoints = await Promise.all(
+            checkpoints.map(async (cp) => {
+              const signed = await handlers.sign(Transaction.fromPSBT(cp), [0])
+              return base64.encode(signed.toPSBT())
+            }),
+          )
+          return {
+            signedTx: base64.encode(signedTx.toPSBT()),
+            signedCheckpoints,
+          }
+        },
       }),
     [handlers],
   )
 
   const poll = useCallback(() => {
     if (iframeRef.current?.contentWindow) {
-      console.log(`[wallet] sending keep alive to ${src}...`)
+      // console.log(`[wallet] sending keep alive to ${src}...`)
       iframeRef.current.contentWindow.postMessage({ kind: 'ARKADE_KEEP_ALIVE', timestamp: Date.now() }, src)
       setTimeout(() => poll(), 5000)
     }
