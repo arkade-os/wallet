fbc7b20383f7d59687aa68e0c80bb74c5d5d7dd3
João Bordalo <bordalix@users.noreply.github.com>
2025-10-18 19:33:59 +0100

    remove console.log

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -6,7 +6,7 @@ setupIonicReact()
 
 // Silence noisy console output while preserving console identity
 beforeEach(() => {
-  // vi.spyOn(console, 'log').mockImplementation(() => {})
+  vi.spyOn(console, 'log').mockImplementation(() => {})
   vi.spyOn(console, 'warn').mockImplementation(() => {})
   vi.spyOn(console, 'error').mockImplementation(() => {})
 })
