a23e6e4160b495fdd4584f6c6d464b7560e58c24
Pietro Grandi <pietro@bluelabs.eu>
2025-10-19 18:58:09 +0200

    Fix CodeRabbit issues

--- src/icons/Escrow.tsx
+++ src/icons/Escrow.tsx
@@ -1,3 +1,3 @@
 export default function EscrowIcon() {
-  return <img height='55' width='55' src='/escrow.png' style={{ borderRadius: '50%' }} />
+  return <img alt='Escrow' height='55' width='55' src='/escrow.png' style={{ borderRadius: '50%' }} />
 }
