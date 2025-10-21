ab004dfddbafb7f8e340f9ea9662700d32ac88ad
Shubert Munthali <shubertm.m@gmail.com>
2025-10-21 15:12:15 +0200

    Add URLs to ENV vars with network type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,8 +4,26 @@
 interface ImportMetaEnv {
   readonly BASE_URL: string
   readonly VITE_SENTRY_DSN?: string
-  readonly VITE_ARK_SERVER?: string
-  readonly VITE_BOLTZ_URL?: string
+  readonly VITE_ARK_SERVER_URL_BITCOIN?: string
+  readonly VITE_ARK_SERVER_URL_TESTNET?: string
+  readonly VITE_ARK_SERVER_URL_MUTINYNET?: string
+  readonly VITE_ARK_SERVER_URL_REGTEST?: string
+  readonly VITE_ARK_SERVER_URL_SIGNET?: string
+  readonly VITE_BOLTZ_URL_BITCOIN?: string
+  readonly VITE_BOLTZ_URL_TESTNET?: string
+  readonly VITE_BOLTZ_URL_MUTINYNET?: string
+  readonly VITE_BOLTZ_URL_REGTEST?: string
+  readonly VITE_BOLTZ_URL_SIGNET?: string
+  readonly VITE_ESPLORA_URL_BITCOIN?: string
+  readonly VITE_ESPLORA_URL_TESTNET?: string
+  readonly VITE_ESPLORA_URL_MUTINYNET?: string
+  readonly VITE_ESPLORA_URL_REGTEST?: string
+  readonly VITE_ESPLORA_URL_SIGNET?: string
+  readonly VITE_INDEXER_URL_BITCOIN?: string
+  readonly VITE_INDEXER_URL_TESTNET?: string
+  readonly VITE_INDEXER_URL_MUTINYNET?: string
+  readonly VITE_INDEXER_URL_REGTEST?: string
+  readonly VITE_INDEXER_URL_SIGNET?: string
   // Add other env variables as needed
 }
 
