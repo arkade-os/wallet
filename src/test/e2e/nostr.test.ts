cdc4078f8e59c06f3c9893193034dd484307d4d4
João Bordalo <bordalix@users.noreply.github.com>
2025-12-26 15:54:04 +0000

    fix nostr test

--- src/test/e2e/nostr.test.ts
+++ src/test/e2e/nostr.test.ts
@@ -88,6 +88,7 @@ test('should save config to nostr', async ({ page }) => {
   await page.getByText('Continue').click()
   await expect(page.getByText('Wallet restored successfully!')).toBeVisible()
   await page.getByText('Go to wallet').click()
+  await page.waitForTimeout(1000)
 
   // verify fiat currency is euro
   await page.getByTestId('tab-settings').click()
@@ -145,8 +146,8 @@ test('should save swaps to nostr', async ({ page, isMobile }) => {
   // should be visible in Boltz app
   await page.getByTestId('tab-apps').click()
   await expect(page.getByText('Boltz', { exact: true })).toBeVisible()
-  await page.getByTestId('app-Boltz').click()
-  await expect(page.getByRole('button', { name: 'Lightning to Arkade + 1,992' })).toBeVisible()
+  await page.getByTestId('app-boltz').click()
+  await expect(page.getByText('+ 1,992 SATS', { exact: true })).toBeVisible()
 
   // transaction should be visible on main page
   await page.getByTestId('tab-wallet').click()
@@ -176,8 +177,8 @@ test('should save swaps to nostr', async ({ page, isMobile }) => {
   // should be visible in Boltz app
   await page.getByTestId('tab-apps').click()
   await expect(page.getByText('Boltz', { exact: true })).toBeVisible()
-  await page.getByTestId('app-Boltz').click()
-  await expect(page.getByRole('button', { name: 'Arkade to Lightning - 1,001' })).toBeVisible()
+  await page.getByTestId('app-boltz').click()
+  await expect(page.getByText('- 1,001 SATS', { exact: true })).toBeVisible()
 
   // transaction should be visible on main page
   await page.getByTestId('tab-wallet').click()
@@ -218,7 +219,7 @@ test('should save swaps to nostr', async ({ page, isMobile }) => {
   // should be visible in Boltz app
   await page.getByTestId('tab-apps').click()
   await expect(page.getByText('Boltz', { exact: true })).toBeVisible()
-  await page.getByTestId('app-Boltz').click()
-  await expect(page.getByRole('button', { name: 'Arkade to Lightning - 1,001' })).toBeVisible()
-  await expect(page.getByRole('button', { name: 'Lightning to Arkade + 1,992' })).toBeVisible()
+  await page.getByTestId('app-boltz').click()
+  await expect(page.getByText('- 1,001 SATS', { exact: true })).toBeVisible()
+  await expect(page.getByText('+ 1,992 SATS', { exact: true })).toBeVisible()
 })
