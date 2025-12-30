e0e991e2eb1d96b468aa5c736af816eeefaa107b
Pietro Grandi <pietro.grandi.3d@gmail.com>
2025-12-30 07:58:47 +0100

    Rollback wallet navigation and Apps updates

--- src/screens/Init/Connect.tsx
+++ src/screens/Init/Connect.tsx
@@ -13,28 +13,27 @@ import { consoleError } from '../../lib/logs'
 export default function InitConnect() {
   const { initInfo, setInitInfo } = useContext(FlowContext)
   const { navigate } = useContext(NavigationContext)
-  const { initWallet, initReadonlyWallet, svcWallet, reloadWallet, initialized } = useContext(WalletContext)
+  const { initWallet, initReadonlyWallet } = useContext(WalletContext)
 
   const { password, privateKey, publicKey } = initInfo
 
   useEffect(() => {
-    if (svcWallet && initialized) {
-      reloadWallet().then(() => navigate(Pages.Wallet))
-    }
-  }, [svcWallet, initialized])
-
-  useEffect(() => {
     if (publicKey) {
-      console.log('init readonly wallet with public key', publicKey)
       initReadonlyWallet(publicKey)
-        .then(() => setInitInfo({ ...initInfo, publicKey: undefined }))
+        .then(() => {
+          setInitInfo({ ...initInfo, publicKey: undefined })
+          navigate(Pages.Wallet)
+        })
         .catch(consoleError)
       return
     }
     if (!password || !privateKey) return
     setPrivateKey(privateKey, password)
       .then(() => initWallet(privateKey))
-      .then(() => setInitInfo({ ...initInfo, password: '', privateKey: undefined }))
+      .then(() => {
+        setInitInfo({ ...initInfo, password: '', privateKey: undefined })
+        navigate(Pages.Wallet)
+      })
       .catch(consoleError)
   }, [])
 
