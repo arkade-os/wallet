d357a0abf674e6ec3bae88c45b2ef7d53e51a25a
João Bordalo <bordalix@users.noreply.github.com>
2026-03-10 13:07:31 +0000

    fix SVGs

--- src/icons/Hash.tsx
+++ src/icons/Hash.tsx
@@ -4,9 +4,9 @@ export default function HashIcon() {
       <path
         fill='none'
         stroke='currentColor'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='2'
         d='M5 9h14M5 15h14M11 4L7 20M17 4l-4 16'
       />
     </svg>
