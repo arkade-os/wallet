85ac3d591c34aaf30df2529a3823691b83b2bd81
João Bordalo <bordalix@users.noreply.github.com>
2026-03-23 15:08:33 +0000

    fix testid casing

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -19,7 +19,7 @@ interface TextProps {
   right?: boolean
   smaller?: boolean
   small?: boolean
-  testid?: string
+  testId?: string
   thin?: boolean
   tiny?: boolean
   tooltip?: string
@@ -41,7 +41,7 @@ export default function Text({
   right,
   smaller,
   small,
-  testid,
+  testId,
   thin,
   tiny,
   tooltip,
@@ -76,7 +76,7 @@ export default function Text({
   }
 
   return (
-    <IonText data-testid={testid}>
+    <IonText data-testid={testId}>
       <p className={className} onClick={handleClick} style={pStyle} title={tooltip}>
         {children}
       </p>
