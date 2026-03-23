9ba95fdcf0bdc09da3b1f81c3221ddac0f019b71
João Bordalo <bordalix@users.noreply.github.com>
2026-03-23 10:48:19 +0000

    adds support to JPY and CNY

--- src/providers/fiat.tsx
+++ src/providers/fiat.tsx
@@ -6,16 +6,18 @@ import { CurrencyDisplay, Fiats, Satoshis } from '../lib/types'
 import { ConfigContext } from './config'
 
 type FiatContextProps = {
+  toFiat: (satoshis?: Satoshis) => number
   fromFiat: (fiat?: number) => Satoshis
-  toFiat: (sats?: Satoshis) => number
+  fiatDecimals: () => number
   updateFiatPrices: () => void
 }
 
-const emptyFiatPrices: FiatPrices = { eur: 0, usd: 0, chf: 0 }
+const emptyFiatPrices: FiatPrices = { eur: 0, usd: 0, chf: 0, jpy: 0, gbp: 0, cny: 0 }
 
 export const FiatContext = createContext<FiatContextProps>({
-  fromFiat: () => 0,
   toFiat: () => 0,
+  fromFiat: () => 0,
+  fiatDecimals: () => 2,
   updateFiatPrices: () => {},
 })
 
@@ -29,19 +31,35 @@ export const FiatProvider = ({ children }: { children: ReactNode }) => {
   const fromEUR = (fiat = 0) => toSatoshis(Decimal.div(fiat, fiatPrices.current.eur).toNumber())
   const fromUSD = (fiat = 0) => toSatoshis(Decimal.div(fiat, fiatPrices.current.usd).toNumber())
   const fromCHF = (fiat = 0) => toSatoshis(Decimal.div(fiat, fiatPrices.current.chf).toNumber())
+  const fromJPY = (fiat = 0) => toSatoshis(Decimal.div(fiat, fiatPrices.current.jpy).toNumber())
+  const fromGBP = (fiat = 0) => toSatoshis(Decimal.div(fiat, fiatPrices.current.gbp).toNumber())
+  const fromCNY = (fiat = 0) => toSatoshis(Decimal.div(fiat, fiatPrices.current.cny).toNumber())
   const toEUR = (sats = 0) => Decimal.mul(fromSatoshis(sats), fiatPrices.current.eur).toNumber()
   const toUSD = (sats = 0) => Decimal.mul(fromSatoshis(sats), fiatPrices.current.usd).toNumber()
   const toCHF = (sats = 0) => Decimal.mul(fromSatoshis(sats), fiatPrices.current.chf).toNumber()
+  const toJPY = (sats = 0) => Decimal.mul(fromSatoshis(sats), fiatPrices.current.jpy).toNumber()
+  const toGBP = (sats = 0) => Decimal.mul(fromSatoshis(sats), fiatPrices.current.gbp).toNumber()
+  const toCNY = (sats = 0) => Decimal.mul(fromSatoshis(sats), fiatPrices.current.cny).toNumber()
 
   const fromFiat = (fiat = 0) => {
     if (config.fiat === Fiats.EUR) return fromEUR(fiat)
     if (config.fiat === Fiats.CHF) return fromCHF(fiat)
+    if (config.fiat === Fiats.JPY) return fromJPY(fiat)
+    if (config.fiat === Fiats.GBP) return fromGBP(fiat)
+    if (config.fiat === Fiats.CNY) return fromCNY(fiat)
     return fromUSD(fiat)
   }
   const toFiat = (sats = 0) => {
     if (config.fiat === Fiats.EUR) return toEUR(sats)
     if (config.fiat === Fiats.CHF) return toCHF(sats)
+    if (config.fiat === Fiats.JPY) return toJPY(sats)
+    if (config.fiat === Fiats.GBP) return toGBP(sats)
+    if (config.fiat === Fiats.CNY) return toCNY(sats)
     return toUSD(sats)
+  }
+
+  const fiatDecimals = () => {
+    return [Fiats.JPY, Fiats.CNY].includes(config.fiat) ? 0 : 2
   }
 
   const updateFiatPrices = async () => {
@@ -57,5 +75,7 @@ export const FiatProvider = ({ children }: { children: ReactNode }) => {
     updateFiatPrices()
   }, [])
 
-  return <FiatContext.Provider value={{ fromFiat, toFiat, updateFiatPrices }}>{children}</FiatContext.Provider>
+  return (
+    <FiatContext.Provider value={{ fromFiat, toFiat, fiatDecimals, updateFiatPrices }}>{children}</FiatContext.Provider>
+  )
 }
