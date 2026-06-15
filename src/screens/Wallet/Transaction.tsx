a359054543ff410a10a85696ff8c07c431a0581a
João Bordalo <bordalix@users.noreply.github.com>
2026-06-15 10:28:44 +0100

    remove 'complete boarding' button

--- src/screens/Wallet/Transaction.tsx
+++ src/screens/Wallet/Transaction.tsx
@@ -35,7 +35,7 @@ export default function Transaction() {
   const issuanceTx = tx ? isIssuance(tx) : false
   const burnTx = tx ? isBurn(tx) : false
   const boardingTx = Boolean(tx?.boardingTxid)
-  const defaultButtonLabel = boardingTx ? 'Complete boarding' : 'Settle transaction'
+  const defaultButtonLabel = 'Settle transaction'
   const boardingExitDelay = Number(aspInfo?.boardingExitDelay || 0)
   const unconfirmedBoardingTx = boardingTx && !tx?.createdAt
   const expiredBoardingTx =
@@ -48,7 +48,6 @@ export default function Transaction() {
   const [hasInputsToSettle, setHasInputsToSettle] = useState(false)
   const [reminderIsOpen, setReminderIsOpen] = useState(false)
   const [settleSuccess, setSettleSuccess] = useState(false)
-  const [resending, setResending] = useState(false)
   const [settling, setSettling] = useState(false)
   const [startTime, setStartTime] = useState(0)
 
@@ -76,15 +75,6 @@ export default function Transaction() {
       setAmountAboveDust(totalAmount > aspInfo.dust)
     })
   }, [aspInfo, vtxos, svcWallet, vtxoManager, wallet.thresholdMs])
-
-  // TODO implement resend
-  //  - create new boarding tx
-  //  - update txInfo with new boarding txid
-  //  - show message that new boarding tx has been created
-  //  - if error, show error message
-  const handleResend = async () => {
-    setResending(true)
-  }
 
   const handleSettle = async () => {
     setError('')
@@ -202,11 +192,7 @@ export default function Transaction() {
     !settling
 
   const Buttons = () =>
-    expiredBoardingTx ? (
-      <ButtonsOnBottom>
-        <Button onClick={handleResend} label='Resend' disabled={resending || true} />
-      </ButtonsOnBottom>
-    ) : showSettleButtons ? (
+    showSettleButtons ? (
       <>
         <ButtonsOnBottom>
           <Button onClick={handleSettle} label={buttonLabel} disabled={settling} />
