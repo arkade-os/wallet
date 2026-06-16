5c523814e94132f95de31ff7e73ac7a2ecb8f649
João Bordalo <bordalix@users.noreply.github.com>
2026-06-16 15:29:26 +0100

    fix tests for "slow" preimage

--- src/test/e2e/swap.test.ts
+++ src/test/e2e/swap.test.ts
@@ -1,3 +1,4 @@
+import { sleep } from '../../lib/sleep'
 import { prettyLongText } from '../../lib/format'
 import {
   test,
@@ -124,7 +125,7 @@ test('should send funds to Lightning', async ({ page }) => {
   await expect(page.getByText('Swap ID')).toBeVisible()
   await expect(page.getByText('Direction')).toBeVisible()
   await expect(page.getByText('Date')).toBeVisible()
-  await expect(page.getByText('Preimage')).toBeVisible()
+  await expect(page.getByText('Preimage')).not.toBeVisible()
   await expect(page.getByText('Invoice')).toBeVisible()
   await expect(page.getByText('Status')).toBeVisible()
   await expect(page.getByText('Amount')).toBeVisible()
@@ -137,6 +138,12 @@ test('should send funds to Lightning', async ({ page }) => {
   expect(await page.getByTestId('Amount').textContent()).toBe('1,000 SATS')
   expect(await page.getByTestId('Fees').textContent()).toBe('1 SAT')
   expect(await page.getByTestId('Total').textContent()).toBe('1,001 SATS')
+
+  // go back, await for swap to settle and preimage to be visible
+  await page.getByLabel('Go back').click()
+  await sleep(3000) // wait for swap to settle
+  await page.getByText('Arkade to Lightning').click()
+  await expect(page.getByText('Preimage')).toBeVisible()
 })
 
 test('should send funds to Bitcoin', async ({ page, isMobile }) => {
