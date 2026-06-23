87a5771f39d789ee4898d3539cc3d260b7cdbe51
João Bordalo <bordalix@users.noreply.github.com>
2026-06-23 14:38:10 +0100

    fix hand written lnurl (#696)
    * fix hand written lnurl
    
    * rabbit fixes
    
    * fixes + tests
    
    * rabbit fix

--- src/test/e2e/swap.test.ts
+++ src/test/e2e/swap.test.ts
@@ -125,7 +125,7 @@ test('should send funds to Lightning', async ({ page }) => {
   await expect(page.getByText('Swap ID')).toBeVisible()
   await expect(page.getByText('Direction')).toBeVisible()
   await expect(page.getByText('Date')).toBeVisible()
-  await expect(page.getByText('Invoice')).toBeVisible()
+  await expect(page.getByText('Invoice', { exact: true })).toBeVisible()
   await expect(page.getByText('Status')).toBeVisible()
   await expect(page.getByText('Amount')).toBeVisible()
   await expect(page.getByText('Fees')).toBeVisible()
