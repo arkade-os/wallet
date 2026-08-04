910e2214293595d4f7995d56b0f61b010b6cce04
gringokiwi <9779954+gringokiwi@users.noreply.github.com>
2026-08-03 23:57:08 -0300

    feat: disable 'server' settings row on mainnet

--- src/screens/Settings/Advanced.tsx
+++ src/screens/Settings/Advanced.tsx
@@ -5,13 +5,17 @@ import Content from '../../components/Content'
 import { SettingsOptions, SettingsSections } from '../../lib/types'
 import Menu from '../../components/Menu'
 import { DevModeContext } from '../../providers/devMode'
+import { AspContext } from '../../providers/asp'
+import { isMainnet } from '../../lib/constants'
 import Padded from '../../components/Padded'
 
 export default function Advanced() {
   const { devMode } = useContext(DevModeContext)
+  const { aspInfo } = useContext(AspContext)
   const rows = options
     .filter((o) => o.section === SettingsSections.Advanced)
     .filter((o) => o.option !== SettingsOptions.Contracts || devMode)
+    .filter((o) => o.option !== SettingsOptions.Server || !isMainnet(aspInfo.network))
 
   return (
     <>
